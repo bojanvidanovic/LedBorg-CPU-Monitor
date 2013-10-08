e591c894667f2cb413a6caf6cabcf6fd8eeccd20
Bojan Vidanovic <web.bojan.vidanovic@gmail.com>
2013-10-08 08:47:42 +0200

    Update ledborg-cpu-monitor.py

--- ledborg-cpu-monitor.py
+++ ledborg-cpu-monitor.py
@@ -9,15 +9,11 @@ import psutil
 colors = ['001','002','012','022','021','020','120','220','210','100','200']
 
 
-# Main function
+# Infinite Loop
 while True:
 
     # Current CPU usage
-    current = int(psutil.cpu_percent(interval=0))
-    if current < 10:
-        current = 0
-    elif current >= 10:
-        current = int(current / 10)
+    current = int(psutil.cpu_percent(interval=0) / 10)
 
     # LedBorg
     LedBorg = open('/dev/ledborg', 'w')
